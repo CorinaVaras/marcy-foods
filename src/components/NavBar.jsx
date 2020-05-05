d6052dca63c4843a23b8b2de40e3828b5aaca529
Gonzalo9725 <varasgonzalo97@gmail.com>
2020-05-05 15:37:15 -0400

    href fixed in delivery

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,70 +1,86 @@
-import React from 'react';
-import menu from '../assets/img/menuIcon.png';
-import '../assets/css/Navbar.css';
-import '../assets/css/hover-min.css';
-import logo from '../assets/img/logo.png';
-import { Link } from 'react-scroll';
+import React from "react";
+import menu from "../assets/img/menuIcon.png";
+import "../assets/css/Navbar.css";
+import "../assets/css/hover-min.css";
+import logo from "../assets/img/logo.png";
+import { Link } from "react-scroll";
 
 const NavBar = () => {
+  return (
+    <>
+      <label htmlFor="toggle">
+        <img
+          style={{ width: "40px", height: "40px", padding: "10px" }}
+          alt="menuIcon"
+          src={menu}
+        />
+      </label>
+      <input type="checkbox" id="toggle" />
 
-    return (
-        <>
-        <label htmlFor="toggle">
-            <img style={{ width: "40px", height: "40px", padding:'10px' }} alt="menuIcon" src={menu}/>
-         </label>
-        <input type="checkbox" id="toggle" />
+      <div className="container-nav">
+        <div className="container-img">
+          <img
+            alt="marcyfood"
+            src={logo}
+            style={{ width: "85px", height: "50px" }}/>
+        </div>
+        <div className="container-items">
+          <div className="item-nav ">
+            <Link
+              className="hvr-underline-from-center"
+              activeClass="active"
+              to="inicio"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={600}
+            >
+              Inicio
+            </Link>
+          </div>
+          <div className="item-nav">
+            <Link
+              className="hvr-underline-from-center"
+              activeClass="active"
+              to="menu"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={600}
+            >
+              Menú
+            </Link>
+          </div>
+          <div className="item-nav">
+            <Link
+              className="hvr-underline-from-center"
+              activeClass="active"
+              to="delivery"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={600}
+            >
+              Delivery
+            </Link>
+          </div>
+          <div className="item-nav">
+            <Link
+              className="hvr-underline-from-center"
+              activeClass="active"
+              to="contacto"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={600}
+            >
+              Contacto
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-        <div className='container-nav'>
-        <div className='container-img'>
-        
-            <img alt="marcyfood" src={logo} style={{width: '85px', height:'50px'}}/>
-            
-        </div>
-        <div className='container-items'> 
-            <div className='item-nav '>
-            <Link
-            className='hvr-underline-from-center'
-            activeClass="active"
-            to="inicio"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={600}>Inicio</Link>
-            </div>
-            <div className='item-nav'>
-            <Link
-            className='hvr-underline-from-center'
-            activeClass="active"
-            to="menu"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={600}>Menú</Link>
-            </div>
-            <div className='item-nav'>
-            <Link
-            className='hvr-underline-from-center'
-            activeClass="active"
-            to="delivery"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={600}>Delivery</Link>
-            </div>
-            <div className='item-nav'>
-            <Link
-            className='hvr-underline-from-center'
-            activeClass="active"
-            to="contacto"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={600}>Contacto</Link>
-            </div>
-        </div>
-        </div>
-        </>
-    )
-}
-
-export default NavBar
+export default NavBar;
